3ff4cf56a35058e299dd5daf1615a0d3823eb645
zinou <maouchi.ziane@yahoo.col>
2019-04-02 15:15:03 +0100

    Voici la première étape est terminée

--- src/main/java/dz/gouv/mesrs/ManagedBean/EquipementBean.java
+++ src/main/java/dz/gouv/mesrs/ManagedBean/EquipementBean.java
@@ -3,18 +3,19 @@ package dz.gouv.mesrs.ManagedBean;
 
 import dz.gouv.mesrs.model.Equipement;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.context.annotation.Scope;
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import java.util.List;
 
-@Setter
-@Getter
+
 @Data
 @Named("equipementBean")
-@Scope("request")
+@Scope("session")
+@ViewScoped
 
 public class EquipementBean extends  BaseBean{
 
@@ -23,27 +24,36 @@ public class EquipementBean extends  BaseBean{
 
     private Equipement equipement;
 
-    @Override
     @PostConstruct
     public void init() {
         equipement = new Equipement();
+        getAll();
     }
 
     @PostConstruct
-
-    public void getAll(){
+     public void getAll(){
         equipements = equipementService.findAll();
+
 
     }
 
     public void add(){
+           System.out.println( equipement.getLabelEquipement());
            equipementService.save(equipement);
+           equipement = new Equipement();
+        equipements = equipementService.findAll();
+        FacesContext.getCurrentInstance().addMessage
+                (null, new FacesMessage(FacesMessage.SEVERITY_INFO, "l'equipempent est enregistré", null));
     }
 
 
     public void remove(Equipement eqp) {
                 equipementService.remove(eqp);
+        equipements = equipementService.findAll();
+        FacesContext.getCurrentInstance().addMessage
+                (null, new FacesMessage(FacesMessage.SEVERITY_INFO, "l'equipempent est supprimé", null));
 
     }
+
 
 }
